38d9aba0e133a2e83cc89f6203877ad300cfed77
Yuanuox <yuanuox@outlook.com>
2022-08-02 10:44:35 +0800

    修复动态匹配词条仅单字时无效的问题

--- src/main/java/org/appxi/dictionary/DictEntryExpr.java
+++ src/main/java/org/appxi/dictionary/DictEntryExpr.java
@@ -33,23 +33,27 @@ public enum DictEntryExpr {
             return entryExpr;
         }
 
-        if (keywords.length() > 2) {
-            if (keywords.charAt(0) == '"' && keywords.charAt(keywords.length() - 1) == '"'
-                || keywords.charAt(0) == '“' && keywords.charAt(keywords.length() - 1) == '”') {
+        // 表达式至少有两个字符
+        if (keywords.length() > 1) {
+            if (keywords.charAt(0) == '"' && keywords.charAt(keywords.length() - 1) == '"' // 以英文双引号包含
+                || keywords.charAt(0) == '“' && keywords.charAt(keywords.length() - 1) == '”' // 以中文双引号包含
+                || keywords.charAt(0) == '\'' && keywords.charAt(keywords.length() - 1) == '\'' // 英文单引号包含
+                || keywords.charAt(0) == '‘' && keywords.charAt(keywords.length() - 1) == '’' // 以中文单引号包含
+            ) {
                 entryExpr = DictEntryExpr.TitleEquals;
                 keywords.deleteCharAt(0);
                 keywords.deleteCharAt(keywords.length() - 1);
-            } else if (keywords.charAt(0) == '*' && keywords.charAt(keywords.length() - 1) == '*') {
+            } else if (keywords.charAt(0) == '*' && keywords.charAt(keywords.length() - 1) == '*') { // 以英文星号包含
                 entryExpr = DictEntryExpr.TitleContains;
                 keywords.deleteCharAt(0);
                 keywords.deleteCharAt(keywords.length() - 1);
-            } else if (keywords.charAt(0) == '*') {
+            } else if (keywords.charAt(0) == '*') { // 以英文星号开始
                 entryExpr = DictEntryExpr.TitleEndsWith;
                 keywords.deleteCharAt(0);
-            } else if (keywords.charAt(keywords.length() - 1) == '*') {
+            } else if (keywords.charAt(keywords.length() - 1) == '*') { // 以英文星号结尾
                 keywords.deleteCharAt(keywords.length() - 1);
             }
-        } else if ("*".equals(keywords.toString())) {
+        } else if (keywords.length() > 0 && keywords.charAt(0) == '*') { // 仅英文星号
             keywords.deleteCharAt(0);
         }
         return entryExpr;
